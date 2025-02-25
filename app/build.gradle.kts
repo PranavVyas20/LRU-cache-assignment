a41917a468a3f6bbfbe33d9c0e27c2db5c90d901
pranavvyas <pranav@changejar.in>
2025-02-26 04:20:12 +0530

    setup dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,20 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.hiltAndroid)
+    id("androidx.room")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.example.lrucacheassignment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lrucacheassignment"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +41,9 @@ android {
     buildFeatures {
         compose = true
     }
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
@@ -49,11 +56,49 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.coroutines)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.androidx.room.paging)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    implementation (libs.ktor.client.android)
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.3")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
+
+
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+
+    implementation ("androidx.compose.material:material-icons-extended:1.7.7")
+
+    implementation ("androidx.navigation:navigation-compose:$2.8.6")
+
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
+
+    debugImplementation ("com.github.chuckerteam.chucker:library:4.1.0")
+    releaseImplementation ("com.github.chuckerteam.chucker:library-no-op:4.1.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.8")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.8")
+
+    implementation("androidx.paging:paging-compose:3.3.5")
+    implementation("androidx.paging:paging-runtime:3.3.5")
     debugImplementation(libs.androidx.ui.tooling)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.hiltAndroid) apply false
+    id("androidx.room") version "2.6.1" apply false
+    kotlin("jvm").version(libs.versions.kotlin).apply(false)
+    kotlin("plugin.serialization").version(libs.versions.kotlin).apply(false)
 }
